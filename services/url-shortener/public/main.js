6c345828bbe60aad50960f865dc77024effb0096
Sophia Brandt <git@s-br.net>
2020-01-28 16:09:29 +0100

    🐛 FIX: fix dockerfile and error alert message

--- services/url-shortener/public/main.js
+++ services/url-shortener/public/main.js
@@ -14,7 +14,7 @@ const renderAlert = (state = 'error') => {
   }
 
   return `
-  <figure class="alert" data-state="success">
+  <figure class="alert" data-state="${state}">
     <svg class="alert__icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="1em" height="1em" viewBox="0 0 24 24">
       <path fill="currentColor" d="${iconPaths[state]}"/>
     </svg>
