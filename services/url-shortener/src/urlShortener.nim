b782e605a2f1ca6c4d004a00e03d90910a3898bd
Sophia Brandt <git@s-br.net>
2020-01-31 14:14:49 +0100

    👌 IMPROVE: improve nginx and deploy setup

--- services/url-shortener/src/urlShortener.nim
+++ services/url-shortener/src/urlShortener.nim
@@ -8,22 +8,26 @@ db.setup()
 settings:
   port = Port(parseInt(os.getEnv("PORT")))
 
+template corsResp(code, message: untyped): untyped =
+  mixin resp
+  resp code, {"Access-Control-Allow-Origin": "*"}, message
+
 routes:
   get "/":
-    resp renderMain(renderShortenUrl())
+    corsResp(Http200, renderMain(renderShortenUrl()))
 
   post "/shortenurl":
     let orig_url = @"orig_url"
     let id = db.shorten(orig_url)
     if not id.len > 0:
       halt "Something went wrong"
-    resp renderMain(renderId(id))
+    corsResp(Http200, renderMain(renderId(id)))
 
   get "/@id":
     cond '.' notin @"id"
     let orig_url = db.getOrigUrl(@"id")
     if orig_url.len == 0:
-      resp renderMain(renderNotFound())
+      corsResp(Http200, renderMain(renderNotFound()))
     else:
       redirect orig_url
 
