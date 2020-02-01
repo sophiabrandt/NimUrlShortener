bf7e79f2ec0bd457eea8d1928a2e32cf62609143
Sophia Brandt <git@s-br.net>
2020-02-01 20:24:38 +0100

    👌 IMPROVE: add meta data to html head

--- services/url-shortener/src/views/general.nim
+++ services/url-shortener/src/views/general.nim
@@ -5,6 +5,9 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Nim URL Shortener</title>
     <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
     <link rel="stylesheet" type="text/css" href="style.css">
