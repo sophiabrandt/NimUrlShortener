2754dd03f11a1c28721736dac7db1317eeb8148c
Sophia Brandt <git@s-br.net>
2020-02-03 17:25:21 +0100

    🐛 FIX: fix regex

--- services/url-shortener/public/main.js
+++ services/url-shortener/public/main.js
@@ -27,10 +27,7 @@ const init = () => {
   const urlElement = document.querySelector('#url')
   const formElement = document.querySelector('#inputForm')
   const alertElement = document.querySelector('[role="alert"]')
-  const validationRegex = new RegExp(
-    '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})',
-    'i'
-  )
+  const validationRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/gm
 
   urlElement && urlElement.removeAttribute('required')
   urlElement && urlElement.removeAttribute('pattern')
